1f760eb9db0e9ea7b3ddf29b57c4b242c1fa700e
LeMinhLong <63239982+LeMinhLong0106@users.noreply.github.com>
2022-10-28 21:09:23 +0700

    date validation with rule

--- resto_app/app/Http/Requests/ReservationStoreRequest.php
+++ resto_app/app/Http/Requests/ReservationStoreRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\DateBetween;
+use App\Rules\TimeBetween;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReservationStoreRequest extends FormRequest
@@ -27,8 +29,7 @@ class ReservationStoreRequest extends FormRequest
             'first_name' => ['required'],
             'last_name' => ['required'],
             'email' => ['required', 'email'],
-            'res_date' => ['required', 'date'],
-            // 'res_date' => ['required', 'date', new DateBetween, new TimeBetween],
+            'res_date' => ['required', 'date', new DateBetween, new TimeBetween],
             'tel_number' => ['required'],
             'table_id' => ['required'],
             'guest_number' => ['required'],
